665190b17d2ed0b160aa32c5c984be0a869381dc
mingchen@ahaent.com <mingchen@ahaent.com>
2023-01-18 17:43:28 +0800

    maint:简化map组件配置

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,8 +2,6 @@
 // 获取小程序全局唯一的App实例
 const app = getApp()
 
-let rewardedVideoAd = null
-
 Page({
   data: {
     user: {
@@ -44,8 +42,8 @@ Page({
         data: result,
         header: {
           'content-type': 'application/json',
-          'openid':app.globalData.openid,
-          'sessionKey':app.globalData.sessionKey,
+          'openid': app.globalData.openid,
+          'sessionKey': app.globalData.sessionKey,
         },
         success(res) {
           console.log('上传用户位置成功' + res)
@@ -62,12 +60,18 @@ Page({
       })
       console.log(app.globalData)
     })
+    // 接收socket消息
+    wx.onSocketMessage((res) => {
+      console.log('接受服务端socket消息', res)
+      // 更新marker的customCallout
+      // @todo 判断与socket返回的用户的id一直的marker.id
 
+    })
   },
   // 页面渲染完成
   onReady() {
     wx.connectSocket({
-      url: 'ws://127.0.0.1:7770/ws?user_id='+app.globalData.openid,
+      url: 'ws://127.0.0.1:7770/ws?user_id=' + app.globalData.openid,
       success: function () {
         console.log('socket 连接成功')
       },
@@ -91,10 +95,6 @@ Page({
         },
       })
     })
-    wx.onSocketMessage((res) => {
-      console.log('接受服务端socket消息', res)
-      // 获取marker中的用户，修改marker中的数据
-    })
   },
   // 选择用户头像
   onChooseAvatar(e) {
@@ -114,4 +114,45 @@ Page({
   },
   // 页面卸载
   onUnload() {},
+  // 用户点击事件处理
+  bindTap() {
+    const that = this
+    // 拉取周围用户最新数据
+    wx.request({
+      url: 'http://localhost:7777/v1/users/geo',
+      method: "GET",
+      data: {
+        latitude: this.data.user.latitude,
+        longitude: this.data.user.longitude,
+        diatance: 100, //@todo 由地图缩放进行计算
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'openid': app.globalData.openid,
+        'sessionKey': app.globalData.sessionKey,
+      },
+      success(res) {
+        console.log('拉取周围其他用户成功' + res)
+        // @todo res更新markers
+        this.markers = [{
+          id: 1,
+          clusterId: 0,
+          joinCluster: true,
+          latitude: that.data.user.latitude,
+          longitude: that.data.user.longitude,
+          zIndex: 20,
+          iconPath: that.data.avatarUrl, //用户自定义头像
+          alpha:0.5,
+          customCallout:{
+
+          },//自定义气泡
+          label: '❤️你一万年',
+        }]
+      },
+      fail(res) {
+        console.log('拉取周围其他用户失败' + res)
+        // 根据状态码，选择重新登陆
+      }
+    })
+  },
 })
